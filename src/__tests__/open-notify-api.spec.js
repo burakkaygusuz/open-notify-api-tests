82317d64ee1a9912dc7d03041bb9a6b1fd504540
Burak Kaygusuz <kaygusuzburak@gmail.com>
2020-09-25 15:32:09 +0300

    Arrow functions were used

--- src/__tests__/open-notify-api.spec.js
+++ src/__tests__/open-notify-api.spec.js
@@ -1,24 +1,24 @@
 const request = require('supertest');
-const {describe, it} = require('@jest/globals');
+const { describe, it } = require('@jest/globals');
 
-describe('International Space Station', function () {
-    it('Get Position', function () {
+describe('International Space Station', () => {
+    it('Get Position', () => {
         response('/iss-now.json')
             .then(res => { res.body.message }, 'success');
     });
-    it('Get Pass Times', function () {
+    it('Get Pass Times', () => {
         response('/iss-pass.json')
             .query({ lat: '50', lon: '100' })
             .expect('Content-Type', /json/)
             .expect(200);
     });
-    it('Get People in Space', function () {
+    it('Get People in Space', () => {
         response('/astros.json')
             .expect('Content-Type', /json/)
             .expect(200);
     });
 });
 
-const response = function (route) {
+const response = (route) => {
     return request('http://api.open-notify.org').get(route);
 };
